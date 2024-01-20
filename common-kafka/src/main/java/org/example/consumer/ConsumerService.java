467e95abf0a5f47df6c92b57bad8d2cdb7a08db3
CarlosBotelho42 <carlospiece55@gmail.com>
2024-01-20 13:03:57 -0300

    feat: implmentatndo e entendo o latest e earliest

--- common-kafka/src/main/java/org/example/consumer/ConsumerService.java
+++ common-kafka/src/main/java/org/example/consumer/ConsumerService.java
@@ -4,10 +4,11 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.example.Message;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public interface ConsumerService<T> {
 
-     void parse(ConsumerRecord<String, Message<T>> record) throws IOException;
+     void parse(ConsumerRecord<String, Message<T>> record) throws IOException, ExecutionException, InterruptedException;
 
      String getTopic();
 
--- sevice-email-new-order/src/main/java/org/example/EmailNewOrderService.java
+++ sevice-email-new-order/src/main/java/org/example/EmailNewOrderService.java
@@ -1,57 +1,45 @@
 package org.example;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.example.consumer.ConsumerService;
 import org.example.consumer.KafkaService;
+import org.example.consumer.ServiceRunner;
 import org.example.dispacher.KafkaDispatcher;
 
 import java.math.BigDecimal;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-public class EmailNewOrderService {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var fraudService = new EmailNewOrderService();
-        try (var service = new KafkaService<>(EmailNewOrderService.class.getSimpleName(),
-                "ECOMMERCE_NEW_ORDER",
-                fraudService::parse,
-                Map.of())) {
-            service.run();
-        }
+public class EmailNewOrderService implements ConsumerService<Order> {
+    public static void main(String[] args) {
+       new ServiceRunner(EmailNewOrderService::new).start(1);
     }
 
     private final KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<>();
 
-    private void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
+    public void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
         System.out.println("Processando novo pedido, checando email...");
-        System.out.println(record.key());
-        System.out.println(record.value());
-        System.out.println(record.partition());
-        System.out.println(record.offset());
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
         var message = record.value();
+        System.out.println(message);
 
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            // ignoring
-            e.printStackTrace();
-        }
         var order = message.getPayload();
-
         var emailCode = "Obrigado pelo seu pedido! Estamos processando se email!";
-        var orderMessage = record.value();
-        String email = orderMessage.getPayload().getEmail();
-        var id = orderMessage.getId().continueWith(EmailNewOrderService.class.getSimpleName());
+        var id = message.getId().continueWith(EmailNewOrderService.class.getSimpleName());
 
         emailDispatcher.send("ECOMMERCE_SEND_EMAIL",
-                email,
+                order.getEmail(),
                 id,
                 emailCode);
+    }
+
+    @Override
+    public String getTopic() {
+        return "ECOMMERCE_NEW_ORDER";
+    }
+
+    @Override
+    public String getConsumerGroup() {
+        return EmailNewOrderService.class.getSimpleName();
     }
 
     private static boolean isFraud(Order order) {
